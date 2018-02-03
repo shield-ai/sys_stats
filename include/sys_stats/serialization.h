b466109410f4a369b27a52488cea0003037f7df2
Andrew Reiter <andrew.reiter@shield.ai>
2018-02-03 11:46:23 -0800

    Wrap library with node that publishes SysStats ROS messages

---
+++ include/sys_stats/serialization.h
@@ -0,0 +1,110 @@
+#ifndef SERIALIZATION_H
+#define SERIALIZATION_H
+
+#include <ros/serialization.h>
+#include <ros/message_traits.h>
+#include <sys_stats/sys_stats.h>
+
+#define TOPIC_TRAITS_FROM_MESSAGE(serializableClass, msg) \
+  ROS_IMPLEMENT_SIMPLE_TOPIC_TRAITS(                      \
+      serializableClass, MD5Sum<msg>::value(), DataType<msg>::value(), Definition<msg>::value())
+
+// Associate library classes with ROS messages
+TOPIC_TRAITS_FROM_MESSAGE(sys_stats::Cpu, shield_msgs::Cpu)
+TOPIC_TRAITS_FROM_MESSAGE(sys_stats::Net, shield_msgs::Net)
+TOPIC_TRAITS_FROM_MESSAGE(sys_stats::Disk, shield_msgs::Disk)
+TOPIC_TRAITS_FROM_MESSAGE(sys_stats::Thread, shield_msgs::Thread)
+TOPIC_TRAITS_FROM_MESSAGE(sys_stats::Process, shield_msgs::Process)
+TOPIC_TRAITS_FROM_MESSAGE(sys_stats::SysStats, shield_msgs::SysStats)
+
+// Define how to read & write each message
+namespace ros
+{
+namespace serialization
+{
+template <>
+struct Serializer<sys_stats::Cpu>
+{
+  ROS_DECLARE_ALLINONE_SERIALIZER template <typename Stream, typename T>
+  inline static void allInOne(Stream& stream, T m)
+  {
+    // must match order in message definition
+    stream.next(m.cpu_frequency);
+    stream.next(m.cpu_temperature);
+    stream.next(m.cpu_usage);
+  }
+};
+
+template <>
+struct Serializer<sys_stats::Net>
+{
+  ROS_DECLARE_ALLINONE_SERIALIZER template <typename Stream, typename T>
+  inline static void allInOne(Stream& stream, T m)
+  {
+    // must match order in message definition
+    stream.next(m.interface);
+    stream.next(m.read);
+    stream.next(m.write);
+  }
+};
+
+template <>
+struct Serializer<sys_stats::Process>
+{
+  ROS_DECLARE_ALLINONE_SERIALIZER template <typename Stream, typename T>
+  inline static void allInOne(Stream& stream, T m)
+  {
+    // must match order in message definition
+    stream.next(m.pid);
+    stream.next(m.cpu_use);
+    stream.next(m.mem_use);
+    stream.next(m.process_name);
+    stream.next(m.args);
+    stream.next(m.threads);
+  }
+};
+
+template <>
+struct Serializer<sys_stats::Thread>
+{
+  ROS_DECLARE_ALLINONE_SERIALIZER template <typename Stream, typename T>
+  inline static void allInOne(Stream& stream, T m)
+  {
+    // must match order in message definition
+    stream.next(m.tid);
+    stream.next(m.cpu_use);
+    stream.next(m.thread_name);
+  }
+};
+
+template <>
+struct Serializer<sys_stats::Disk>
+{
+  ROS_DECLARE_ALLINONE_SERIALIZER template <typename Stream, typename T>
+  inline static void allInOne(Stream& stream, T m)
+  {
+    // must match order in message definition
+    stream.next(m.disk_name);
+    stream.next(m.read);
+    stream.next(m.write);
+  }
+};
+
+template <>
+struct Serializer<sys_stats::SysStats>
+{
+  ROS_DECLARE_ALLINONE_SERIALIZER template <typename Stream, typename T>
+  inline static void allInOne(Stream& stream, T m)
+  {
+    // must match order in message definition
+    stream.next(m.cpu_use_total);
+    stream.next(m.processes);
+    stream.next(m.cpu);
+    stream.next(m.interface);
+    stream.next(m.disks);
+  }
+};
+}
+}
+
+#endif  // SERIALIZATION_H
---
+++ src/sys_stats_node.cpp
@@ -0,0 +1,44 @@
+#include <ros/ros.h>
+#include <shield_msgs/SysStats.h>
+#include <sys_stats/sys_stats.h>
+#include <sys_stats/serialization.h>
+
+class SysStatsWrapper
+{
+ public:
+  SysStatsWrapper()
+  {
+    ros::NodeHandle nh;
+    ros::NodeHandle nhp("~");
+
+    pub_ = nh.advertise<shield_msgs::SysStats>("sys_stats", 0);
+    timer_ = nh.createTimer(ros::Rate(nhp.param("rate", 1.0)), &SysStatsWrapper::getStats, this);
+  }
+
+  void getStats(const ros::TimerEvent&)
+  {
+    if (sys_stats::get_sys_stats(&stats_))
+    {
+      pub_.publish(stats_);
+    }
+    else
+    {
+      ROS_WARN("Could not get system statistics");
+    }
+  }
+
+  sys_stats::SysStats stats_;
+  ros::Timer timer_;
+  ros::Publisher pub_;
+};
+
+int main(int argc, char* argv[])
+{
+  ros::init(argc, argv, ROS_PACKAGE_NAME);
+
+  SysStatsWrapper stats_wrapper;
+
+  ros::spin();
+
+  return 0;
+}
